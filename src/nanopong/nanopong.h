7ca9bdba5be835a48e1c6991be6a0d0dc8c9f6df
Gervais Pierre <94633856+GervaisPierre@users.noreply.github.com>
2023-03-15 13:34:22 +0100

    added header for updatePlayer() & updateBall

--- src/nanopong/nanopong.h
+++ src/nanopong/nanopong.h
@@ -2,7 +2,7 @@
 
 #include <nanolib.h>
 
-typedef struct s_game_grid {
+typedef struct s_game_grid {    //May be deprecate. TBD.
     //The ball will be on a 6x8 grid.
     //X being 6 in length
     //Padle is locked from 1-6 but still writes from 0-7.
@@ -10,11 +10,16 @@ typedef struct s_game_grid {
     //The frame being the "image" displayed one the LED matrix.
     bool playerPaddle1[8];
     bool playerPaddle2[8];
-    bool ball[6][8];
+    bool ball[8][8];
 } t_game_grid;
 
 typedef struct ball {
-    int direction;      //Defined by: e_ball_direction.
+    //Defined by: e_ball_direction.
+    int direction;      
+    //Top left corner is 0,0.
+    //Bottom right corner is 7,7.
+    int x;  //Min = 0, Max = 7
+    int y;  //Min = 0, Max = 7
 } t_ball;
 
 typedef enum e_ball_direction {
@@ -30,4 +35,9 @@ typedef struct s_player {
     int paddlePosition; //Min = 0, Max = 6
     int score;          //Min = 0, Max = 3
 } t_player;
+
+
+void updatePlayer(t_player player, t_nano_pin potentiometer);
+
+void updateBall(t_ball ball, t_game_grid gameGrid);
 
