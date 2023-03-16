62cf8aef961725e5a0b3d5318a50f786b830c4b6
NYAMSI10 <b.nyamsi@yahoo.com>
2023-03-16 09:16:00 +0100

    ok

--- src/nanopong/nanopong.c
+++ src/nanopong/nanopong.c
@@ -1,8 +1,20 @@
 #include <avr/io.h>
 #include <stdio.h>
+#include "nanopong.h"
+#include "nanopong/nanopong.h"
+
+
 
 int main(){
+      
+      enum PaddlePosition  position = 0;
 
+      if (position)
+      {
+         
+      }
+      
+      
 
     return 0 ;
 }
